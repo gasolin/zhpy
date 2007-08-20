070e4d11adca7a9b1d8f7fb092b5daee3c47b485
gasolin <gasolin@localhost>
2007-08-20 01:19:35 +0000

    pump up version and remove redundent files

--- n_inout.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-p0 = raw_input("輸入姓名: ")
-print "歡迎, %s"%p0
--- setup.py
+++ setup.py
@@ -19,12 +19,14 @@ setup(
     author=author,
     author_email=email,
     license=license,
+    keywords = "traditional, simplified, chinese",
     description=description,
     long_description=long_description,
     url=url,
     zip_safe=False,
     install_requires = install_requires,
-    packages=find_packages(),
+    include_package_data = True,
+    packages=find_packages(exclude=["ez_setup"]),
     entry_points = """
     [console_scripts]
     zhpy = zhpy.zhpy:commandtool
@@ -33,6 +35,7 @@ setup(
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Education',
+        'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: Chinese (Traditional)',
         'Natural Language :: Chinese (Simplified)',
