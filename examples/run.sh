e419083a11ef2c49cc5e5136a39784434aa3bc45
gasolin <gasolin@localhost>
2007-08-20 10:02:34 +0000

    add example test runner

---
+++ examples/run.sh
@@ -0,0 +1,23 @@
+echo "zhpy hello example"
+
+cd hello
+zhpy hello.py
+cd ..
+
+echo "zhpy in/out example"
+cd inout
+zhpy inout.py
+cd ..
+
+echo "zhpy loop example"
+cd loop
+zhpy tabl.py
+cd ..
+
+echo "zhpy game example, pygame module required. Use 'Ctrl+c' to escape"
+
+cd game
+zhpy game.py
+cd ..
+
+echo "finish"
