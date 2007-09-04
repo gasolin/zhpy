4797f14406650a58d22efcf293b0164dae55ace0
gasolin <gasolin@localhost>
2007-09-04 07:22:24 +0000

    add zhpy_ext sample plugin template

---
+++ zhpy_ext/README.txt
@@ -0,0 +1,10 @@
+README
+---------
+
+An example plugin project for zhpy
+
+see 
+
+http://code.google.com/p/zhpy/wiki/ModulePlugin
+
+for detail.
---
+++ zhpy_ext/word.py
@@ -0,0 +1,12 @@
+#coding=utf-8
+
+"""
+zh_module plugin
+"""
+
+
+#enter traditional chinese dict here
+tw_dict = {"traditional":"english"}
+
+#enter simplified chinese dict here
+cn_dict = {"Simplified":"english"}
