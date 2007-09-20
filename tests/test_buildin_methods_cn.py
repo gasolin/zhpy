59434f217df8aa9891a7a1ec8f8126c36142f3f6
gasolin <gasolin@localhost>
2007-09-20 06:47:36 +0000

    merge test files

--- tests/test_buildin_methods_cn.py
+++ tests/test_buildin_methods_cn.py
@@ -16,3 +16,17 @@ def test_len():
     test len method
     """
     assert convertor("长度('hello')") == "len('hello')"
+
+def test_range():
+    """
+    test preload moudle: range
+    """
+    assert convertor("取 i 在 范围(10): 打印 i") == "for i in range(10): print i"
+
+def test_enumerate():
+    """
+    test enumerate
+    
+    """
+    assert convertor("取 (index, item) 在 列举(items): 打印 index, item") == \
+                    "for (index, item) in enumerate(items): print index, item"
