0c3a62406a96d4945ec34136b398a58932ba2f9a
gasolin <gasolin@localhost>
2007-10-12 02:24:15 +0000

    update release note

--- zhpy/release.py
+++ zhpy/release.py
@@ -18,23 +18,23 @@ long_description = """
 Introduction
 -------------
 
-zhpy is the full feature python language with 
-fully tested chinese keywords, variables, and parameters support, 
-independent on python version,
-bundle with command line tool, interpreter, pluggable keyword system and 
-great document.
+zhpy is the full feature python language with fully tested chinese
+keywords, variables, and parameters support, independent on python
+version, bundle with command line tool, interpreter, pluggable
+keyword system and great document.
 
-zhpy on python is good for Taiwan and China beginners to learn python in 
+zhpy on python is good for Taiwan and China beginners to learn python in
 our native language.
 
-The core of zhpy is a lightweight python module and a chinese source convertor
-based on python, 
-which provides interpreter and command line tool to translate zhpy code to
-python. zhpy integrated a setuptools-based plugin system for keyword reuse.
-The zhpy code written in traditional and simplified
-chinese could be translated and converted to natual python code.
-Thus it could be execute as nature python code and be used in normal
-python programs.
+The core of zhpy is a lightweight python module and a chinese
+source convertor based on python, which provides interpreter and
+command line tool to translate zhpy code to python.
+
+zhpy integrated a setuptools-based plugin system for keyword reuse.
+The zhpy code written in traditional and simplified chinese could be
+translated and converted to natual python code.
+Thus it could be execute as nature python code and be used in
+normal python programs.
 
 Use 'zhpy' command instead of "python" in command line to execute source
 code wrote in Chinese and English.
@@ -76,6 +76,7 @@ Programming Guide
 
 An C.C licensed zhpy book "A Byte of Zhpy" is available on site.
 You could freely view it online.
+
   * http://code.google.com/p/zhpy/wiki/ByteOfZhpy
 
 The book is based on "A Byte of python".
@@ -88,6 +89,6 @@ Change Log
 -------------
 
 You could view the ChangeLog to see what's new in these version.
-http://zhpy.googlecode.com/svn/trunk/CHANGELOG.txt
-
+  * http://zhpy.googlecode.com/svn/trunk/CHANGELOG.txt
+  
 """
