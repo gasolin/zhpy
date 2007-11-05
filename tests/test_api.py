5235e479e89c4264d5832f32302f44758a3e9e99
renhbo <renhbo@localhost>
2007-11-05 15:01:01 +0000

    Add test for multi lines triple quota and p_??_v test cases.

--- tests/test_api.py
+++ tests/test_api.py
@@ -16,6 +16,13 @@ def test_convertor():
     assert python_convertor(convertor("印出 'hello'")) == ("印出 'hello'")
     # check if variable convert correctly, not a python valid syntax
     assert python_convertor(convertor("打出 '''哈囉'''")) == ("打出 '''哈囉'''")
+    assert python_convertor(convertor("打出 '''p_哈囉_v'''")) == ("打出 '''p_哈囉_v'''")
+    assert python_convertor(convertor("打出 '''哈\
+    囉'''")) == ("打出 '''哈\
+    囉'''")
+    assert python_convertor(convertor('打出 """哈\
+    _囉"""')) == ('打出 """哈\
+    _囉"""')
     
 
 def test_uri():
@@ -23,5 +30,6 @@ def test_uri():
     test uri
     """
     assert zh_ord('範例'.decode("utf8")) == 'p_7bc4_4f8b_v'
+    assert zh_ord('p_範例_v'.decode("utf8")) == 'p_70_5f_7bc4_4f8b_5f_76_v'
     assert zh_chr('p_7bc4_4f8b_v') == '範例'
     assert zh_chr(zh_ord('範例'.decode("utf8"))) == '範例'
