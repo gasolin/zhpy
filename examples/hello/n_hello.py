83f5aca41eb4480db253a2e79b4f679f362bc09d
gasolin <gasolin@localhost>
2008-04-12 04:56:18 +0000

    example update

--- examples/hello/n_hello.py
+++ examples/hello/n_hello.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # this is a sample Python program
-def p_7bc4_4f8b_v_1(p_8a0a_606f_v,p_6b21_6578_v):
+def p_7bc4_4f8b_v_1(p_8a0a_606f_v, p_6b21_6578_v):
    for p_6578_v in range(p_6b21_6578_v):
        print "哈囉, %s!" % p_8a0a_606f_v
 
