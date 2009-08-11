525e7e54ca39727fb41998482b42c190cf85e7ae
gasolin <gasolin@localhost>
2009-08-11 07:15:23 +0000

    add description

--- examples/droid/android.py
+++ examples/droid/android.py
@@ -1,4 +1,8 @@
 class Android(object):
+    """
+    The mock up of 
+    http://code.google.com/p/android-scripting/source/browse/python/ase/android.py
+    """
     def __init__(self):
         print "create Android ok"
     
